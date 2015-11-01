626fce190f3a22eaf6f7cc83a308e121f757d7f3
https://github.com/Infinity-420 <iceis2@hotmail.com>
2015-10-31 23:56:48 -0400

    assignment chp2 # 5 -- quick easy mailing label
    Signed-off-by: https://github.com/Infinity-420 <iceis2@hotmail.com>

---
+++ Case,Julia 2008/Chtper2a5/Chtper2a5/MailingAddressForm.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Chtper2a5
+{
+    partial class MailingAddressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.FirstNametextBox = new System.Windows.Forms.TextBox();
+            this.LastNametextBox = new System.Windows.Forms.TextBox();
+            this.StreetAddresstextBox = new System.Windows.Forms.TextBox();
+            this.CitytextBox = new System.Windows.Forms.TextBox();
+            this.StatetextBox = new System.Windows.Forms.TextBox();
+            this.DisplayInfobutton = new System.Windows.Forms.Button();
+            this.Clearbutton = new System.Windows.Forms.Button();
+            this.Printbutton = new System.Windows.Forms.Button();
+            this.Exitbutton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ZipCodemaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.MailingLabelgroupBox = new System.Windows.Forms.GroupBox();
+            this.MailingLabelOutPuttextBox = new System.Windows.Forms.TextBox();
+            this.MailingLabelgroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Street Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "City";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "State";
+            // 
+            // FirstNametextBox
+            // 
+            this.FirstNametextBox.Location = new System.Drawing.Point(112, 44);
+            this.FirstNametextBox.Name = "FirstNametextBox";
+            this.FirstNametextBox.Size = new System.Drawing.Size(100, 20);
+            this.FirstNametextBox.TabIndex = 5;
+            // 
+            // LastNametextBox
+            // 
+            this.LastNametextBox.Location = new System.Drawing.Point(112, 74);
+            this.LastNametextBox.Name = "LastNametextBox";
+            this.LastNametextBox.Size = new System.Drawing.Size(100, 20);
+            this.LastNametextBox.TabIndex = 6;
+            // 
+            // StreetAddresstextBox
+            // 
+            this.StreetAddresstextBox.Location = new System.Drawing.Point(112, 101);
+            this.StreetAddresstextBox.Name = "StreetAddresstextBox";
+            this.StreetAddresstextBox.Size = new System.Drawing.Size(100, 20);
+            this.StreetAddresstextBox.TabIndex = 7;
+            // 
+            // CitytextBox
+            // 
+            this.CitytextBox.Location = new System.Drawing.Point(112, 130);
+            this.CitytextBox.Name = "CitytextBox";
+            this.CitytextBox.Size = new System.Drawing.Size(100, 20);
+            this.CitytextBox.TabIndex = 8;
+            // 
+            // StatetextBox
+            // 
+            this.StatetextBox.Location = new System.Drawing.Point(112, 155);
+            this.StatetextBox.Name = "StatetextBox";
+            this.StatetextBox.Size = new System.Drawing.Size(100, 20);
+            this.StatetextBox.TabIndex = 9;
+            // 
+            // DisplayInfobutton
+            // 
+            this.DisplayInfobutton.Location = new System.Drawing.Point(52, 231);
+            this.DisplayInfobutton.Name = "DisplayInfobutton";
+            this.DisplayInfobutton.Size = new System.Drawing.Size(75, 23);
+            this.DisplayInfobutton.TabIndex = 10;
+            this.DisplayInfobutton.Text = "Display Info";
+            this.DisplayInfobutton.UseVisualStyleBackColor = true;
+            this.DisplayInfobutton.Click += new System.EventHandler(this.DisplayInfobutton_Click);
+            // 
+            // Clearbutton
+            // 
+            this.Clearbutton.Location = new System.Drawing.Point(52, 260);
+            this.Clearbutton.Name = "Clearbutton";
+            this.Clearbutton.Size = new System.Drawing.Size(75, 23);
+            this.Clearbutton.TabIndex = 11;
+            this.Clearbutton.Text = "Clear";
+            this.Clearbutton.UseVisualStyleBackColor = true;
+            this.Clearbutton.Click += new System.EventHandler(this.Clearbutton_Click);
+            // 
+            // Printbutton
+            // 
+            this.Printbutton.Location = new System.Drawing.Point(52, 289);
+            this.Printbutton.Name = "Printbutton";
+            this.Printbutton.Size = new System.Drawing.Size(75, 23);
+            this.Printbutton.TabIndex = 13;
+            this.Printbutton.Text = "&Print";
+            this.Printbutton.UseVisualStyleBackColor = true;
+            // 
+            // Exitbutton
+            // 
+            this.Exitbutton.Location = new System.Drawing.Point(52, 318);
+            this.Exitbutton.Name = "Exitbutton";
+            this.Exitbutton.Size = new System.Drawing.Size(75, 23);
+            this.Exitbutton.TabIndex = 12;
+            this.Exitbutton.Text = "E&xit";
+            this.Exitbutton.UseVisualStyleBackColor = true;
+            this.Exitbutton.Click += new System.EventHandler(this.Exitbutton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Zip Code";
+            // 
+            // ZipCodemaskedTextBox
+            // 
+            this.ZipCodemaskedTextBox.Location = new System.Drawing.Point(112, 182);
+            this.ZipCodemaskedTextBox.Mask = "00000-9999";
+            this.ZipCodemaskedTextBox.Name = "ZipCodemaskedTextBox";
+            this.ZipCodemaskedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ZipCodemaskedTextBox.TabIndex = 15;
+            // 
+            // MailingLabelgroupBox
+            // 
+            this.MailingLabelgroupBox.Controls.Add(this.MailingLabelOutPuttextBox);
+            this.MailingLabelgroupBox.Location = new System.Drawing.Point(286, 32);
+            this.MailingLabelgroupBox.Name = "MailingLabelgroupBox";
+            this.MailingLabelgroupBox.Size = new System.Drawing.Size(511, 280);
+            this.MailingLabelgroupBox.TabIndex = 16;
+            this.MailingLabelgroupBox.TabStop = false;
+            this.MailingLabelgroupBox.Text = "Mailing Label:";
+            // 
+            // MailingLabelOutPuttextBox
+            // 
+            this.MailingLabelOutPuttextBox.Location = new System.Drawing.Point(42, 37);
+            this.MailingLabelOutPuttextBox.Multiline = true;
+            this.MailingLabelOutPuttextBox.Name = "MailingLabelOutPuttextBox";
+            this.MailingLabelOutPuttextBox.ReadOnly = true;
+            this.MailingLabelOutPuttextBox.Size = new System.Drawing.Size(431, 228);
+            this.MailingLabelOutPuttextBox.TabIndex = 0;
+            this.MailingLabelOutPuttextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MailingAddressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 411);
+            this.Controls.Add(this.MailingLabelgroupBox);
+            this.Controls.Add(this.ZipCodemaskedTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Printbutton);
+            this.Controls.Add(this.Exitbutton);
+            this.Controls.Add(this.Clearbutton);
+            this.Controls.Add(this.DisplayInfobutton);
+            this.Controls.Add(this.StatetextBox);
+            this.Controls.Add(this.CitytextBox);
+            this.Controls.Add(this.StreetAddresstextBox);
+            this.Controls.Add(this.LastNametextBox);
+            this.Controls.Add(this.FirstNametextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MailingAddressForm";
+            this.Text = "Mailing Address Helper";
+            this.MailingLabelgroupBox.ResumeLayout(false);
+            this.MailingLabelgroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox FirstNametextBox;
+        private System.Windows.Forms.TextBox LastNametextBox;
+        private System.Windows.Forms.TextBox StreetAddresstextBox;
+        private System.Windows.Forms.TextBox CitytextBox;
+        private System.Windows.Forms.TextBox StatetextBox;
+        private System.Windows.Forms.Button DisplayInfobutton;
+        private System.Windows.Forms.Button Clearbutton;
+        private System.Windows.Forms.Button Printbutton;
+        private System.Windows.Forms.Button Exitbutton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox ZipCodemaskedTextBox;
+        private System.Windows.Forms.GroupBox MailingLabelgroupBox;
+        private System.Windows.Forms.TextBox MailingLabelOutPuttextBox;
+    }
+}
+
---
+++ Case,Julia 2008/Chtper2a5/Chtper2a5/MailingAddressForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chtper2a5
+{
+    public partial class MailingAddressForm : Form
+    {
+        public MailingAddressForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Exitbutton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DisplayInfobutton_Click(object sender, EventArgs e)
+        {
+            MailingLabelOutPuttextBox.Text = FirstNametextBox.Text + " " + LastNametextBox.Text + Environment.NewLine + StreetAddresstextBox.Text + Environment.NewLine +
+                                                CitytextBox.Text + ", " + StatetextBox.Text + Environment.NewLine + ZipCodemaskedTextBox.Text;
+        }
+
+        private void Clearbutton_Click(object sender, EventArgs e)
+        {
+            FirstNametextBox.Clear();
+            LastNametextBox.Clear();
+            StreetAddresstextBox.Clear();
+            CitytextBox.Clear();
+            StatetextBox.Clear();
+            ZipCodemaskedTextBox.Clear();
+            FirstNametextBox.Focus();
+
+            // Clear output text box
+            MailingLabelOutPuttextBox.Clear();
+        }
+    }
+}
